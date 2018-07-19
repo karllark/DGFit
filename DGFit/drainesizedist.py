695b24ee72728841864a13ac1699c0af9c6e634d
Karl Gordon <kgordon@stsci.edu>
2018-07-19 15:40:42 -0400

    Getting going.

--- DGFit/drainesizedist.py
+++ DGFit/drainesizedist.py
@@ -4,30 +4,31 @@
 #   from Weingartner & Draine (2001)
 #
 # Started: Jul 2016 (KDG)
-# 
 
 from scipy.special import erf
 import numpy as np
+
 
 def WGsizedist_graphite(a, bC_input, Cg, a_tg, a_cg, alpha_g, beta_g):
     # a in Angstrom and assumed to always be > 3.5 A
 
     # very small gain size distribution
     a0 = np.array([3.5, 30.])   # in A
-    bC = np.array([0.75,0.25])*bC_input
+    bC = np.array([0.75, 0.25])*bC_input
     sigma = 0.4
-    rho = 2.24 # in g/cm^3 for graphite
+    rho = 2.24  # in g/cm^3 for graphite
     mC = 12.0107*1.660e-24
-    
+
     Da = 0.0
     for i in range(2):
-        Bi = (3.0/(np.power(2.0*np.pi,1.5)))* \
-             (np.exp(-4.5*np.power(sigma,2.0))/
-              (rho*np.power(1e-8*a0[i],3.0)*sigma))* \
-             (bC[i]*mC/(1.0 + erf((3.0*sigma/np.sqrt(2.0)) +
-                                  np.log(a0[i]/3.5)/(sigma*np.sqrt(2.0)))))
-        
-        Da += (Bi/(1e-8*a))*np.exp(-0.5*np.power(np.log(a/a0[i])/sigma,2.0))
+        Bi = ((3.0/(np.power(2.0*np.pi, 1.5)))
+              * (np.exp(-4.5*np.power(sigma, 2.0))
+              / (rho*np.power(1e-8*a0[i], 3.0)*sigma))
+              * (bC[i]*mC/(1.0 + erf((3.0*sigma/np.sqrt(2.0))
+                                     + np.log(a0[i]/3.5)/(sigma*np.sqrt(2.0)))
+                           )))
+
+        Da += (Bi/(1e-8*a))*np.exp(-0.5*np.power(np.log(a/a0[i])/sigma, 2.0))
 
     # larger grain size distribution
     if beta_g >= 0.0:
@@ -35,16 +36,17 @@ def WGsizedist_graphite(a, bC_input, Cg, a_tg, a_cg, alpha_g, beta_g):
     else:
         Fa = 1.0/(1.0 - beta_g*a/a_tg)
 
-    Ga = np.full((len(a)),1.0)
+    Ga = np.full((len(a)), 1.0)
     indxs, = np.where(a > a_tg)
-    Ga[indxs] = np.exp(-1.0*np.power((a[indxs] - a_tg)/a_cg,3.0))
+    Ga[indxs] = np.exp(-1.0*np.power((a[indxs] - a_tg)/a_cg, 3.0))
 
-    graphite_sd = Da + (Cg/(1e-8*a))*np.power(a/a_tg,alpha_g)*Fa*Ga
+    graphite_sd = Da + (Cg/(1e-8*a))*np.power(a/a_tg, alpha_g)*Fa*Ga
 
     return graphite_sd
 
+
 if __name__ == "__main__":
-    
+
     import argparse
 
     import matplotlib.pyplot as plt
@@ -65,7 +67,7 @@ if __name__ == "__main__":
 
     # setup the plots
     fontsize = 18
-    font = {'size'   : fontsize}
+    font = {'size': fontsize}
 
     matplotlib.rc('font', **font)
 
@@ -74,22 +76,22 @@ if __name__ == "__main__":
     matplotlib.rc('xtick.major', width=2)
     matplotlib.rc('ytick.major', width=2)
 
-    fig, ax = plt.subplots(figsize=(14,10))
+    fig, ax = plt.subplots(figsize=(14, 10))
 
     # coefficients from Weingartner & Draine for R(V) = 3.1, Case A
-    bC = np.array([0.0, 1.0, 2.0, 3.0, 
+    bC = np.array([0.0, 1.0, 2.0, 3.0,
                    4.0, 5.0, 6.0])*1e-5
     Cg = np.array([9.94e-11, 3.79e-10, 5.57e-11, 4.15e-11,
                    2.90e-11, 3.20e-12, 9.99e-12])
-    atg = np.array([0.00745, 0.00373, 0.00828, 0.00837, 
+    atg = np.array([0.00745, 0.00373, 0.00828, 0.00837,
                     0.00898, 0.0254, 0.0107])*1e4
     acg = np.array([0.606, 0.586, 0.543, 0.499,
                     0.489, 0.438, 0.428])*1e4
-    alpha_g = np.array([-2.25, -2.17, -2.04, -1.91, 
-                         -1.84, -1.72, -1.54])
+    alpha_g = np.array([-2.25, -2.17, -2.04, -1.91,
+                        -1.84, -1.72, -1.54])
     beta_g = np.array([-0.0648, -0.0382, -0.111, -0.125,
-                        -0.132, -0.322, -0.165])
-    
+                       -0.132, -0.322, -0.165])
+
     # get the size distributions and plot
     for i in range(len(bC)):
         sizedist_graphite = WGsizedist_graphite(a,
@@ -100,16 +102,16 @@ if __name__ == "__main__":
                                                 alpha_g[i],
                                                 beta_g[i])
 
-        ax.plot(a*1e-4, sizedist_graphite*np.power(1e-8*a,4.0)*1e29)
+        ax.plot(a*1e-4, sizedist_graphite*np.power(1e-8*a, 4.0)*1e29)
 
     ax.set_xscale('log')
     ax.set_yscale('log')
-    ax.set_xlim(3.5e-4,2.2)
-    ax.set_ylim(0.1,25)
+    ax.set_xlim(3.5e-4, 2.2)
+    ax.set_ylim(0.1, 25)
     ax.set_xlabel(r'$\lambda$ [$\mu m$]')
     ax.set_ylabel(r'$dn/da$ [$10^{-29}$ $cm^3$ $n_H^{-1}$]')
-    
-    plt.tight_layout()    
+
+    plt.tight_layout()
 
     # show or save
     basename = 'drainesizedist'
@@ -121,5 +123,3 @@ if __name__ == "__main__":
         fig.savefig(basename+'.pdf')
     else:
         plt.show()
-    
-    
